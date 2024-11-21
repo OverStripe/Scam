b06a3aa0da356d526dc9a360ba39281936d9887d
Oᴠᴇʀ ❖ Sᴛʀɪᴘᴇ <120897603+OverStripe@users.noreply.github.com>
2024-11-21 09:08:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from telegram import Update, Bot
 from telegram.ext import (
-    Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+    Updater, CommandHandler, CallbackContext, MessageHandler, filters
 )
 import sqlite3
 import os
